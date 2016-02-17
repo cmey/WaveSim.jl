d92534400df6190c9358e9ac2ac3d07363780331
Christophe MEYER <christophe.meyer.pro@gmail.com>
2016-02-17 15:47:12 -0600

    Compute num transducers from delays

--- ultrasim.jl
+++ ultrasim.jl
@@ -1,32 +1,33 @@
-using PyPlot
+#using PyPlot
 #using SIUnits
 #using SIUnits.ShortUnits
 
 # Config
 const c = 1540 # * m / s
 const F0 = 3_000_000 # * Hz
-const end_simulation_time = 10 * 1e-6 # [s] start at 0 s
+const end_simulation_time = 30 * 1e-6 # [s] start at 0 s
 
-const image_res = [128, 128]# * m^-1
-const image_fov = [0.01, 0.01]# * m
-const n_time_steps = 9
+const image_res = [32, 32]# * m^-1
+const image_fov = [0.04, 0.04]# * m
+const n_time_steps = 3
 
-const n_transducers = 5
 pulse_shape_func(phase) = cos(phase)
 const pulse_length = 1 / F0 # [s] 1 complete cycle
-const pitch_transducers = 200 * 1e-6 # [m] # spacing between trans
+const transducer_array_size = 0.03 # [m] # physical size of transducer
 
-function simulate_one_time_step!(image, t, image_pitch, x_transducers, wavelength)
-
-  for x in range(1, image_res[1])
-    for y in range(1, image_res[2])
+function simulate_one_time_step!(image, t, image_pitch, x_transducers, trans_delays, wavelength)
+  println("simulate_one_time_step")
+  for x in 1:image_res[1]
+    for y in 1:image_res[2]
       pix_index = [x, y]
       pix_coord = pix_index .* image_pitch
 
-      for xt in x_transducers
+      for i_trans in 1:length(x_transducers)
+        xt = x_transducers[i_trans]
         trans_coord = [xt, 0]# * m
         dist_to_transducer = norm(trans_coord .- pix_coord)
-        time_to_reach = dist_to_transducer / c
+        trans_delay = trans_delays[i_trans]
+        time_to_reach = dist_to_transducer / c + trans_delay
         if time_to_reach <= t <= time_to_reach+pulse_length
           amp = pulse_shape_func(dist_to_transducer/wavelength*2*pi)
         else
@@ -38,21 +39,42 @@ function simulate_one_time_step!(image, t, image_pitch, x_transducers, wavelengt
   end
 end
 
-function ultrasim()
+function init(trans_delays)
   wavelength = c/F0 # [m]
-  x_transducers = [pitch_transducers*itrans for itrans in range(1, n_transducers)]
+  n_transducers = length(trans_delays)
+  pitch_transducers = transducer_array_size / n_transducers
+  x_transducers = [pitch_transducers*itrans for itrans in 1:n_transducers]
   tvec = linspace(0, float(end_simulation_time), n_time_steps)
   image_pitch = image_fov ./ image_res
-  
-  for t in tvec
-    image = zeros(Float32, (image_res[1], image_res[2]))
-    simulate_one_time_step!(image, t, image_pitch, x_transducers, wavelength)
-
-    figure(1)
-    imshow(image)
-    show()
-  end
-
+  return wavelength, x_transducers, tvec, image_pitch
 end
 
-ultrasim()
+function ultrasim(trans_delays)
+  wavelength, x_transducers, tvec, image_pitch = init(trans_delays)
+  
+  images = []
+
+  for t in tvec
+    image = zeros(Float32, (image_res[1], image_res[2]))
+    simulate_one_time_step!(image, t, image_pitch, x_transducers, trans_delays, wavelength)
+    push!(images, image)
+  end
+
+  return images
+end
+
+using Gadfly
+
+function main()
+  const n_transducers = 5
+  trans_delays = zeros(n_transducers)
+  images = ultrasim(trans_delays)
+  n_sim_time_steps = length(images)
+  for i_image in 1:n_sim_time_steps
+    spy(images[i_image])
+    sleep(1)
+  end
+  return images
+end
+
+#main()
