238d347e43e0629aad2dae458221953ab6e46a2c
Christophe Meyer <christophe.meyer.pro@gmail.com>
2017-06-29 22:40:13 -0400

    Remove bilog by default

--- view.jl
+++ view.jl
@@ -6,9 +6,8 @@ function colorize_field(images)
     num_colormap_entries = 100
     cmap = colormap("RdBu", num_colormap_entries)
 
-    images_bilog = bilog(images)
-    min, max = extrema(images_bilog)
-    images_scaled_for_indexing = clamp.((images_bilog - min) / (max - min) * num_colormap_entries + 1, 1, num_colormap_entries)
+    min, max = extrema(images)
+    images_scaled_for_indexing = clamp.((images - min) / (max - min) * num_colormap_entries + 1, 1, num_colormap_entries)
     images_indexed = Int.(round.(images_scaled_for_indexing))
 
     return cmap[images_indexed]
