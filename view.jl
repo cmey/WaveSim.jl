fc9e70562aeac6e7d232f4ad21c5af4cafba37c2
Christophe Meyer <christophe.meyer.pro@gmail.com>
2017-06-29 21:26:35 -0400

    Colorize visualization

--- view.jl
+++ view.jl
@@ -1,7 +1,20 @@
-include("UltraSim.jl")
+include("bilog.jl")
 using ImageView
+using Colors
 
-using UltraSim
-images = UltraSim.main()
+function colorize_field(images)
+    num_colormap_entries = 100
+    cmap = colormap("RdBu", num_colormap_entries)
 
-imshow(images, axes=(2, 3))
+    images_bilog = bilog(images)
+    min, max = extrema(images_bilog)
+    images_scaled_for_indexing = clamp((images_bilog - min) / (max - min) * num_colormap_entries + 1, 1, num_colormap_entries)
+    images_indexed = Int.(round.(images_scaled_for_indexing))
+
+    return cmap[images_indexed]
+end
+
+function imshow4d(images)
+    imshow(colorize_field(images), axes=(2, 3))
+end
+
