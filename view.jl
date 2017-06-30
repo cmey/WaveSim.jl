ba7dbf4dc6b3c897c8a58c4f3c25ec270a439de8
Christophe Meyer <christophe.meyer.pro@gmail.com>
2017-06-29 21:38:38 -0400

    Fix deprec, use . vec-syntax

--- view.jl
+++ view.jl
@@ -8,7 +8,7 @@ function colorize_field(images)
 
     images_bilog = bilog(images)
     min, max = extrema(images_bilog)
-    images_scaled_for_indexing = clamp((images_bilog - min) / (max - min) * num_colormap_entries + 1, 1, num_colormap_entries)
+    images_scaled_for_indexing = clamp.((images_bilog - min) / (max - min) * num_colormap_entries + 1, 1, num_colormap_entries)
     images_indexed = Int.(round.(images_scaled_for_indexing))
 
     return cmap[images_indexed]
