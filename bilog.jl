e4f8c3fe068d99390224d73cae4a8b2c30e4ac68
Christophe Meyer <christophe.meyer.pro@gmail.com>
2017-06-29 21:09:57 -0400

    Add signed log util bilog.jl

---
+++ bilog.jl
@@ -0,0 +1,16 @@
+# This function creates bipolar (i.e. signed) log of the input.
+
+function bilog(datain, dbrange=30)
+    datain = real.(datain)
+    maxabs = abs.(maximum(datain))
+    minabs = abs.(minimum(datain))
+
+    if minabs > maxabs
+        maxabs = minabs
+    end
+
+    norm_mag = (abs.(datain) + eps()) / maxabs
+    out = sign.(datain) .* (clamp.(20 * log10.(norm_mag), -dbrange, 0) + dbrange)
+
+    return out
+end
