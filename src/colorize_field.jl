39cb996bed9a4ddc4c028523727ac83821188a9c
Christophe Meyer <christophe.meyer.pro@gmail.com>
2017-12-21 01:49:47 +0100

    Split colorize_field.jl off view.jl

---
+++ src/colorize_field.jl
@@ -0,0 +1,14 @@
+using Colors
+
+
+function colorize_field(images)
+    num_colormap_entries = 100
+    cmap = colormap("RdBu", num_colormap_entries)
+
+    min, max = extrema(images)
+    images_scaled_for_indexing = clamp.((images - min) / (max - min) * num_colormap_entries + 1, 1, num_colormap_entries)
+    images_indexed = Int.(round.(images_scaled_for_indexing))
+
+    return cmap[images_indexed]
+end
+
--- src/view.jl
+++ src/view.jl
@@ -1,17 +1,6 @@
 include("bilog.jl")
-using Colors, ImageView
-
-
-function colorize_field(images)
-    num_colormap_entries = 100
-    cmap = colormap("RdBu", num_colormap_entries)
-
-    min, max = extrema(images)
-    images_scaled_for_indexing = clamp.((images - min) / (max - min) * num_colormap_entries + 1, 1, num_colormap_entries)
-    images_indexed = Int.(round.(images_scaled_for_indexing))
-
-    return cmap[images_indexed]
-end
+include("colorize_field.jl")
+using ImageView
 
 
 function imshowall(sim_params, images, beam_energy_map, transmit_time_map, dbrange=40)
