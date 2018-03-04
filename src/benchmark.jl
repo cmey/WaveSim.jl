0240345afcfd807316ae479c247f8694af9334d7
Christophe Meyer <christophe.meyer.pro@gmail.com>
2018-03-04 17:16:34 -0500

    Reduce compute burden in benchmark (used in tests)

--- src/benchmark.jl
+++ src/benchmark.jl
@@ -7,8 +7,8 @@ function main()
     focus_depth = 0.03,  # [m]
     steer_angle = 10.0,  # [deg]
     aperture_size = 0.01,  # [m]
-    temporal_res = 0.1e-6,  # [s]
-    spatial_res = [256, 512]  # [pixels]
+    temporal_res = 0.5e-6,  # [s]
+    spatial_res = [32, 64]  # [pixels]
   )
 
   # Compute focusing delays for the elements of the phased array.
