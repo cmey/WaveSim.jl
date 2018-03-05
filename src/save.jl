514a09c930c11a7ad9376c4fcca4c20bb85784ad
Christophe Meyer <christophe.meyer.pro@gmail.com>
2018-03-04 20:17:43 -0500

    Simplify wave_propagation_filename

--- src/save.jl
+++ src/save.jl
@@ -5,7 +5,7 @@ include("bilog.jl")
 include("colorize_field.jl")
 
 @pyimport matplotlib.animation as anim  # After using PyPlot, so matplotlib installed via conda.
-const wave_propagation_filename = "wave_propagation"
+const wave_propagation_filename = "wave_propagation.mp4"
 const beam_energy_map_filename = "beam_energy_map.png"
 const transmit_time_map_filename = "transmit_time_map.png"
 
@@ -34,7 +34,7 @@ function saveall(images, beam_energy_map, transmit_time_map, sim_params, output_
     # a) ffmpeg is installed with libx264 support, and
     # b) matplotlib is built with ffmpeg support enabled
     ani = anim.ArtistAnimation(fig, ims, interval=30, blit=true, repeat=false)
-    anim_filename = wave_propagation_filename * ".mp4"
+    anim_filename = wave_propagation_filename
     ani[:save](joinpath(output_path, anim_filename),
                extra_args=["-vcodec", "libx264", "-pix_fmt", "yuv420p"])
     #= ani[:save]("ani.gif"); =#
