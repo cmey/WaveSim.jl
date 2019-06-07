c7c7ebc80e8c15fd8c447d69ea9d5360dff0e80c
Christophe Meyer <christophe.meyer.pro@gmail.com>
2019-06-07 16:56:01 -0400

    TEMP disable animation save, is way too slow

--- src/save.jl
+++ src/save.jl
@@ -1,11 +1,12 @@
 using Parameters
-using PyCall
+# using PyCall
 using PyPlot
+# using VideoIO
 include("bilog.jl")
 include("colorize_field.jl")
 
-anim = pyimport("matplotlib.animation")  # After using PyPlot, so matplotlib installed via conda.
-const wave_propagation_filename = "wave_propagation.mp4"
+# anim = pyimport("matplotlib.animation")  # After using PyPlot, so matplotlib installed via conda.
+# const wave_propagation_filename = "wave_propagation.mp4"
 const beam_energy_map_filename = "beam_energy_map.png"
 const transmit_time_map_filename = "transmit_time_map.png"
 
@@ -26,31 +27,30 @@ function saveall(images, beam_energy_map, transmit_time_map, sim_params, output_
         transmit_time_map = rotr90(transmit_time_map)
     end
 
-    wave_field = bilog(images, dbrange)
-    vmin, vmax = extrema(wave_field)
-
-    fig = figure()
-    ims = []
-    for i_time in 1:size(wave_field, 3)
-        im = PyPlot.imshow(wave_field[:, :, i_time], extent=extent, cmap="viridis", vmin=vmin, vmax=vmax)
-        if 1 == i_time
-            PyPlot.colorbar()
-        end
-        PyPlot.title("Wave amplitude [dB]")
-        PyPlot.xlabel(xlabel)
-        PyPlot.ylabel(ylabel)
-        push!(ims, PyCall.PyObject[im])
-    end
-    #= close() =#
-
-    # If matplotlib complains, ensure that
-    # a) ffmpeg is installed with libx264 support, and
-    # b) matplotlib is built with ffmpeg support enabled
-    ani = anim.ArtistAnimation(fig, ims, interval=30, blit=true, repeat=false)
-    anim_filename = wave_propagation_filename
-    ffmpeg_writer = anim.FFMpegWriter()
-    ani.save(joinpath(output_path, anim_filename), ffmpeg_writer)
-    #= ani.save("ani.gif"); =#
+    # wave_field = bilog(images, dbrange)
+    # vmin, vmax = extrema(wave_field)
+    #
+    # fig = figure()
+    # ims = []
+    # for i_time in 1:size(wave_field, 3)
+    #     im = PyPlot.imshow(wave_field[:, :, i_time], extent=extent, cmap="viridis", vmin=vmin, vmax=vmax)
+    #     if 1 == i_time
+    #         PyPlot.colorbar()
+    #     end
+    #     PyPlot.title("Wave amplitude [dB]")
+    #     PyPlot.xlabel(xlabel)
+    #     PyPlot.ylabel(ylabel)
+    #     push!(ims, PyCall.PyObject[im])
+    # end
+    # #= close() =#
+    #
+    # # If matplotlib complains, ensure that
+    # # a) ffmpeg is installed with libx264 support, and
+    # # b) matplotlib is built with ffmpeg support enabled
+    # ani = anim.ArtistAnimation(fig, ims, interval=30, blit=true, repeat=false)
+    # ffmpeg_writer = anim.FFMpegWriter()
+    # ani.save(joinpath(output_path, wave_propagation_filename), ffmpeg_writer, dpi=72)
+    # # ani.save("ani.gif");
 
     PyPlot.figure()
     PyPlot.imshow(bilog(beam_energy_map, dbrange), extent=extent)
