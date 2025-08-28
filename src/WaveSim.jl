ce2c04ef1101c4b103fb4665dbbabad6b0e62a5a
Christophe Meyer <christophe.meyer.pro@gmail.com>
2025-08-28 13:24:10 +0200

    Compute and save time-integrated and peak-to-peak maps

--- src/WaveSim.jl
+++ src/WaveSim.jl
@@ -172,25 +172,28 @@ end
 function beam_energy_map_and_transmit_time_map(images, sim_params)
     @unpack temporal_res = sim_params
 
+    # integrated_energy_map = dropdims(sqrt.(mean(images.^2, dims=3)), dims=3)  # RMS
+    integrated_energy_map = dropdims(mean(abs.(images), dims=3), dims=3)  # MeanAbs
+
+    # peak to peak amplitude
     maxval, maxlinindices = findmax(images, dims=3)
     minval, minlinindices = findmin(images, dims=3)
+    peak_to_peak_map = dropdims(maxval .- minval, dims=3)
 
-    beam_energy_map = dropdims(maxval .- minval, dims=3)  # peak to peak amplitude
-
-    transmit_time_map = similar(beam_energy_map)
+    transmit_time_map = similar(peak_to_peak_map)
     for linind in eachindex(maxlinindices)
         x, y, t = Tuple(CartesianIndices(images)[maxlinindices[linind]])
         transmit_time_map[linind] = t * temporal_res
     end
 
-    peak_to_peak_time_delta_map = similar(beam_energy_map)
+    peak_to_peak_time_delta_map = similar(peak_to_peak_map)
     for linind in eachindex(maxlinindices)
         x, y, tmax = Tuple(CartesianIndices(images)[maxlinindices[linind]])
         x, y, tmin = Tuple(CartesianIndices(images)[minlinindices[linind]])
         peak_to_peak_time_delta_map[linind] = (tmax - tmin) * temporal_res
     end
 
-    return beam_energy_map, transmit_time_map, peak_to_peak_time_delta_map
+    return integrated_energy_map, peak_to_peak_map, transmit_time_map, peak_to_peak_time_delta_map
 end
 
 
--- src/save.jl
+++ src/save.jl
@@ -1,18 +1,35 @@
 using CairoMakie
+using Interpolations
+# using JLD
 using Makie.Colors
 using Parameters
 include("bilog.jl")
 
+const saved_data_filename = "saved_data.jld"
 const wave_propagation_filename = "wave_propagation.gif"
-const beam_energy_map_filename = "beam_energy_map.png"
+const integrated_energy_map_filename = "integrated_energy_map.png"
+const peak_to_peak_map_filename = "peak_to_peak_map.png"
 const transmit_time_map_filename = "transmit_time_map.png"
 const peak_to_peak_time_delta_map_filename = "peak_to_peak_time_delta_map.png"
 const line_scan_filename = "line_scan.png"
+const arc_scan_filename = "arc_scan.png"
+const peak_to_peak_map_with_arc_filename = "peak_to_peak_map_with_arc.png"
 
-function saveall(images, beam_energy_map, transmit_time_map, peak_to_peak_time_delta_map, sim_params, output_path="images")
+function saveall(images, integrated_energy_map, peak_to_peak_map, transmit_time_map, peak_to_peak_time_delta_map, sim_params, output_path="images", conditions_string="")
+    println("Saving simulation results to: ", output_path)
     mkpath(output_path)
 
-    @unpack fov, dbrange, orientation = sim_params
+    function make_filename(original_filename, conditions_string)
+        if isempty(conditions_string)
+            return original_filename
+        else
+            return "$(conditions_string)_$(original_filename)"
+        end
+    end
+
+    # save(joinpath(output_path, saved_data_filename), "images", images; compress=true)
+
+    @unpack tx_frequency, pulse_cycles, fov, dbrange, orientation = sim_params
 
     extent = [0, fov[2], -1/2 * fov[1], 1/2 * fov[1]]
     xlabel = "Depth [m]"
@@ -24,17 +41,104 @@ function saveall(images, beam_energy_map, transmit_time_map, peak_to_peak_time_d
         xlabel, ylabel = ylabel, xlabel
 
         images = mapslices(rotr90, images; dims=[1, 2])
-        beam_energy_map = rotr90(beam_energy_map)
+        integrated_energy_map = rotr90(integrated_energy_map)
+        peak_to_peak_map = rotr90(peak_to_peak_map)
         transmit_time_map = rotr90(transmit_time_map)
         peak_to_peak_time_delta_map = rotr90(peak_to_peak_time_delta_map)
     end
 
+    # Integrated energy map
+    integrated_energy_map = integrated_energy_map';
+    fig = Figure()
+    ax = Axis(fig[1, 1], width=size(integrated_energy_map)[1], height=size(integrated_energy_map)[2], xlabel=xlabel, ylabel=ylabel, title="Time-integrated energy map [dB]\n$(conditions_string)")
+    centers_x = range(extent[1], extent[2], length=size(integrated_energy_map)[1])
+    centers_y = range(extent[3], extent[4], length=size(integrated_energy_map)[2])
+    hm = heatmap!(ax, centers_x, centers_y, bilog(integrated_energy_map, dbrange), colormap=:jet1)
+    Colorbar(fig[:, end+1], hm)
+    resize_to_layout!(fig)
+    save(joinpath(output_path, make_filename(integrated_energy_map_filename, conditions_string)), fig)
+
+    # Peak-to-peak map
+    peak_to_peak_map = peak_to_peak_map';
+    fig = Figure()
+    ax = Axis(fig[1, 1], width=size(peak_to_peak_map)[1], height=size(peak_to_peak_map)[2], xlabel=xlabel, ylabel=ylabel, title="Peak-to-peak map [dB]\n$(conditions_string)")
+    centers_x = range(extent[1], extent[2], length=size(peak_to_peak_map)[1])
+    centers_y = range(extent[3], extent[4], length=size(peak_to_peak_map)[2])
+    hm = heatmap!(ax, centers_x, centers_y, bilog(peak_to_peak_map, dbrange), colormap=:jet1)
+    Colorbar(fig[:, end+1], hm)
+    resize_to_layout!(fig)
+    save(joinpath(output_path, make_filename(peak_to_peak_map_filename, conditions_string)), fig)
+
+    # Transmit time map
+    transmit_time_map = transmit_time_map';
+    fig = Figure()
+    ax = Axis(fig[1, 1], width=size(transmit_time_map)[1], height=size(transmit_time_map)[2], xlabel=xlabel, ylabel=ylabel, title="Transmit time map [µs]\n$(conditions_string)")
+    centers_x = range(extent[1], extent[2], length=size(peak_to_peak_map)[1])
+    centers_y = range(extent[3], extent[4], length=size(peak_to_peak_map)[2])
+    hm = heatmap!(ax, centers_x, centers_y, transmit_time_map .* 1e6, colormap=:jet1)
+    Colorbar(fig[:, end+1], hm)
+    resize_to_layout!(fig)
+    save(joinpath(output_path, make_filename(transmit_time_map_filename, conditions_string)), fig)
+
+    # Peak-to-peak time-delta map
+    peak_to_peak_time_delta_map = peak_to_peak_time_delta_map';
+    max_display_peak_to_peak_duration = 1.0f0 / tx_frequency * pulse_cycles
+    peak_to_peak_time_delta_map = clamp.(peak_to_peak_time_delta_map, -max_display_peak_to_peak_duration, max_display_peak_to_peak_duration)
+    fig = Figure()
+    ax = Axis(fig[1, 1], width=size(peak_to_peak_time_delta_map)[1], height=size(peak_to_peak_time_delta_map)[2], xlabel=xlabel, ylabel=ylabel, title="Peak-to-peak time-delta map [µs]\n$(conditions_string)")
+    centers_x = range(extent[1], extent[2], length=size(peak_to_peak_time_delta_map)[1])
+    centers_y = range(extent[3], extent[4], length=size(peak_to_peak_time_delta_map)[2])
+    # Define custom red–green–red colormap
+    red_green_red = cgrad([:red, :green, :red], 256, categorical = false)
+    hm = heatmap!(ax, centers_x, centers_y, peak_to_peak_time_delta_map .* 1e6, colormap=red_green_red)
+    Colorbar(fig[:, end+1], hm)
+    resize_to_layout!(fig)
+    save(joinpath(output_path, make_filename(peak_to_peak_time_delta_map_filename, conditions_string)), fig)
+
+    # Line scan at end depth
+    line_scan = peak_to_peak_map[end, :]
+    fig = Figure()
+    ax = Axis(fig[1, 1], xlabel="Azimuth [m]", ylabel="Peak-to-peak amplitude [AU]", title="Linear scan - azimuth at $(fov[2]) [m] depth\n$(conditions_string)")
+    centers_x = range(extent[3], extent[4], length=length(line_scan))
+    scatterlines!(ax, centers_x, line_scan)
+    resize_to_layout!(fig)
+    save(joinpath(output_path, make_filename(line_scan_filename, conditions_string)), fig)
+
+    # Arc scan passing through end depth
+    center = (1, size(peak_to_peak_map)[2] / 2)  # (x_center, y_center)
+    radius = min(size(peak_to_peak_map)[1], size(peak_to_peak_map)[2]) / 2 - 1
+    num_points = 200  # number of points along the curve
+    angles = range(-π/2, π/2, length=num_points)
+    x_arc = floor.([center[1] + radius * cos(θ) for θ in angles])
+    y_arc = floor.([center[2] + radius * sin(θ) for θ in angles])
+    itp = interpolate(peak_to_peak_map, BSpline(Linear()))
+    arc_values = [itp(x, y) for (x, y) in zip(x_arc, y_arc)]
+    fig = Figure()
+    ax = Axis(fig[1, 1], xlabel="Angle [degs]", ylabel="Peak-to-peak amplitude [AU]", title="Arc scan - azimuth at $(fov[2]) [m] depth\n$(conditions_string)")
+    scatterlines!(ax, angles / π * 180, arc_values)
+    resize_to_layout!(fig)
+    save(joinpath(output_path, make_filename(arc_scan_filename, conditions_string)), fig)
+
+    # Peak-to-peak map marked with arc
+    fig = Figure()
+    ax = Axis(fig[1, 1], width=size(peak_to_peak_map)[1], height=size(peak_to_peak_map)[2], xlabel=xlabel, ylabel=ylabel, title="Peak-to-peak map [dB]\n$(conditions_string)")
+    centers_x = range(extent[1], extent[2], length=size(peak_to_peak_map)[1])
+    centers_y = range(extent[3], extent[4], length=size(peak_to_peak_map)[2])
+    image = bilog(peak_to_peak_map, dbrange)
+    for (x, y) in zip(x_arc, y_arc)
+        image[round(Int, x), round(Int, y)] = dbrange  # mark the arc
+    end
+    hm = heatmap!(ax, centers_x, centers_y, image)
+    Colorbar(fig[:, end+1], hm)
+    resize_to_layout!(fig)
+    save(joinpath(output_path, make_filename(peak_to_peak_map_with_arc_filename, conditions_string)), fig)
+
     # Wave propagation movie
     images = permutedims(images, (2, 1, 3));
-    wave_field_images = bilog(images, dbrange)
+    wave_field_images = bilog(images, 60)
     vmin, vmax = extrema(wave_field_images)
     fig = Figure()
-    ax = Axis(fig[1, 1], width=size(wave_field_images)[1], height=size(wave_field_images)[2], xlabel=xlabel, ylabel=ylabel, title="Wave amplitude [dB]")
+    ax = Axis(fig[1, 1], width=size(wave_field_images)[1], height=size(wave_field_images)[2], xlabel=xlabel, ylabel=ylabel, title="Wave amplitude [dB]\n$(conditions_string)")
     centers_x = range(extent[1], extent[2], length=size(wave_field_images)[1])
     centers_y = range(extent[3], extent[4], length=size(wave_field_images)[2])
     hm = heatmap!(ax, centers_x, centers_y, wave_field_images[:, :, 1]; colormap=:RdBu_11, colorrange=(vmin, vmax))
@@ -42,53 +146,11 @@ function saveall(images, beam_energy_map, transmit_time_map, peak_to_peak_time_d
     resize_to_layout!(fig)
     framerate = 30  # [fps]
     duration = 3  # [s]
-    time_indices = Int.(round.(range(1, size(wave_field_images)[3], length=framerate*duration)))
-    record(fig, joinpath(output_path, wave_propagation_filename), time_indices; framerate=framerate) do time_index
+    time_indices = round.(Int, range(1, size(wave_field_images)[3], length=framerate*duration))
+    record(fig, joinpath(output_path, make_filename(wave_propagation_filename, conditions_string)), time_indices; framerate=framerate) do time_index
         heatmap!(ax, centers_x, centers_y, wave_field_images[:, :, time_index]; colormap=:RdBu_11, colorrange=(vmin, vmax))
         println("Saving simulation frame ", time_index, "/", size(wave_field_images)[3], " ")
     end
-
-    # Beam energy map
-    beam_energy_map = beam_energy_map';
-    fig = Figure()
-    ax = Axis(fig[1, 1], width=size(beam_energy_map)[1], height=size(beam_energy_map)[2], xlabel=xlabel, ylabel=ylabel, title="Beam energy map [dB]")
-    centers_x = range(extent[1], extent[2], length=size(beam_energy_map)[1])
-    centers_y = range(extent[3], extent[4], length=size(beam_energy_map)[2])
-    hm = heatmap!(ax, centers_x, centers_y, bilog(beam_energy_map, dbrange))
-    Colorbar(fig[:, end+1], hm)
-    resize_to_layout!(fig)
-    save(joinpath(output_path, beam_energy_map_filename), fig)
-
-    # Transmit time map
-    transmit_time_map = transmit_time_map';
-    fig = Figure()
-    ax = Axis(fig[1, 1], width=size(transmit_time_map)[1], height=size(transmit_time_map)[2], xlabel=xlabel, ylabel=ylabel, title="Transmit time map [µs]")
-    centers_x = range(extent[1], extent[2], length=size(beam_energy_map)[1])
-    centers_y = range(extent[3], extent[4], length=size(beam_energy_map)[2])
-    hm = heatmap!(ax, centers_x, centers_y, transmit_time_map .* 1e6)
-    Colorbar(fig[:, end+1], hm)
-    resize_to_layout!(fig)
-    save(joinpath(output_path, transmit_time_map_filename), fig)
-
-    # Peak-to-peak time-delta map
-    peak_to_peak_time_delta_map = peak_to_peak_time_delta_map';
-    fig = Figure()
-    ax = Axis(fig[1, 1], width=size(peak_to_peak_time_delta_map)[1], height=size(peak_to_peak_time_delta_map)[2], xlabel=xlabel, ylabel=ylabel, title="Peak-to-peak time-delta map [µs]")
-    centers_x = range(extent[1], extent[2], length=size(peak_to_peak_time_delta_map)[1])
-    centers_y = range(extent[3], extent[4], length=size(peak_to_peak_time_delta_map)[2])
-    hm = heatmap!(ax, centers_x, centers_y, peak_to_peak_time_delta_map .* 1e6)
-    Colorbar(fig[:, end+1], hm)
-    resize_to_layout!(fig)
-    save(joinpath(output_path, peak_to_peak_time_delta_map_filename), fig)
-
-    # Line scan at end depth
-    line_scan = beam_energy_map'[:, end]
-    fig = Figure()
-    ax = Axis(fig[1, 1], xlabel="Azimuth [m]", ylabel="Peak-to-peak amplitude [AU]", title="Linear scan - azimuth at $(fov[2]) [m] depth")
-    centers_x = range(extent[3], extent[4], length=length(line_scan))
-    scatterlines!(ax, centers_x, line_scan)
-    resize_to_layout!(fig)
-    save(joinpath(output_path, line_scan_filename), fig)
 
     return  # nothing
 end
